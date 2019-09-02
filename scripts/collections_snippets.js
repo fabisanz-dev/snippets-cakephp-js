d7415b0f46226e96cd23177fbe0542f8ca77e631
fabisanz-dev <sjorgefabi@gmail.com>
2019-09-01 23:33:32 -0300

    first commit

---
+++ scripts/collections_snippets.js
@@ -0,0 +1,18 @@
+const array_snippets_cakephp = [
+    {dir: 'cakephp', name: 'prueba1'},
+    {dir: 'cakephp', name: 'prueba2'}
+];
+
+const array_snippets_cakephp_plugins = [
+    {dir: 'cakephp-plugins', name: 'spreadsheet'}
+];
+
+const array_snippets_js = [
+    {dir: 'js', name: 'agrupar'}
+];
+
+const array_snippets_js_plugins = [
+    {dir: 'js-plugins', name: 'bootstrap3-datetimepicker'}
+];
+
+export { array_snippets_cakephp, array_snippets_cakephp_plugins, array_snippets_js, array_snippets_js_plugins }
